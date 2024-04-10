37520ce4f0883fe7ae564d2808a62efe3f848fbe
michalkonecny <mikkonecny@gmail.com>
2024-04-10 11:38:30 +0100

    autoformat

--- src/Numeric/CollectErrors/Type.hs
+++ src/Numeric/CollectErrors/Type.hs
@@ -1,20 +1,29 @@
-{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE ConstraintKinds #-}
-module Numeric.CollectErrors.Type 
+{-# LANGUAGE TypeSynonymInstances #-}
 
-where
-
-import GHC.Generics
-import Control.DeepSeq
-
-import qualified Data.List as List
-import qualified Data.Set as Set
+module Numeric.CollectErrors.Type where
 
 import Control.CollectErrors
-    ( CanTestErrorsCertain(..), CollectErrors (CollectErrors), noValue, removeValue, prependErrors, liftCE, lift2CE, lift1TCE, liftT1CE, unCollectErrors, CanTestErrorsPresent, CanTakeErrors  )
+  ( CanTakeErrors,
+    CanTestErrorsCertain (..),
+    CanTestErrorsPresent,
+    CollectErrors (CollectErrors),
+    lift1TCE,
+    lift2CE,
+    liftCE,
+    liftT1CE,
+    noValue,
+    prependErrors,
+    removeValue,
+    unCollectErrors,
+  )
+import Control.DeepSeq
+import qualified Data.List as List
+import qualified Data.Set as Set
+import GHC.Generics
 
 cn :: v -> CN v
 cn = pure
@@ -23,20 +32,24 @@ unCN :: CN p -> p
 unCN = unCollectErrors
 
 type CN = CollectErrors NumErrors
+
 newtype NumErrors = NumErrors (Set.Set NumErrorLevel)
   deriving (Eq, Semigroup, Monoid, CanTestErrorsCertain, CanTestErrorsPresent, Generic, NFData)
+
 type NumErrorLevel = (NumError, ErrorCertaintyLevel)
 
 instance Show NumErrors where
   show (NumErrors set) =
-    "{" <> (List.intercalate "; " $ map showEL $ Set.toList set)  <>  "}"
+    "{" <> (List.intercalate "; " $ map showEL $ Set.toList set) <> "}"
     where
-    showEL (e,l) =
-      show l <> ": " <> show e
+      showEL (e, l) =
+        show l <> ": " <> show e
 
-data NumError =
-    DivByZero | OutOfDomain String | NumError String
-    deriving (Eq, Ord, Generic)
+data NumError
+  = DivByZero
+  | OutOfDomain String
+  | NumError String
+  deriving (Eq, Ord, Generic)
 
 instance NFData NumError
 
@@ -45,8 +58,9 @@ instance Show NumError where
   show (OutOfDomain s) = "out of domain: " ++ s
   show (NumError s) = "numeric error: " ++ s
 
-data ErrorCertaintyLevel =
-  ErrorCertain | ErrorPotential
+data ErrorCertaintyLevel
+  = ErrorCertain
+  | ErrorPotential
   deriving (Eq, Ord, Generic)
 
 instance NFData ErrorCertaintyLevel
@@ -58,57 +72,57 @@ instance Show ErrorCertaintyLevel where
 instance CanTestErrorsCertain NumErrorLevel where
   hasCertainError = (== ErrorCertain) . snd
 
-{-| Construct an empty wrapper indicating that given error has certainly occurred. -}
+-- | Construct an empty wrapper indicating that given error has certainly occurred.
 noValueNumErrorCertain :: NumError -> CN v
 noValueNumErrorCertain e = noValue $ NumErrors $ Set.singleton (e, ErrorCertain)
 
-{-| Construct an empty wrapper indicating that given error may have occurred. -}
+-- | Construct an empty wrapper indicating that given error may have occurred.
 noValueNumErrorPotential :: NumError -> CN v
 noValueNumErrorPotential e = noValue $ NumErrors $ Set.singleton (e, ErrorPotential)
 
 removeValueErrorCertain :: CN t -> NumError -> CN t
-removeValueErrorCertain v e = 
+removeValueErrorCertain v e =
   removeValue v $ NumErrors $ Set.singleton (e, ErrorCertain)
 
 removeValueErrorPotential :: CN t -> NumError -> CN t
-removeValueErrorPotential v e = 
+removeValueErrorPotential v e =
   removeValue v $ NumErrors $ Set.singleton (e, ErrorPotential)
 
 prependErrorCertain :: NumError -> CN t -> CN t
 prependErrorCertain e = prependErrors $ NumErrors $ Set.singleton (e, ErrorCertain)
-  
+
 prependErrorPotential :: NumError -> CN t -> CN t
 prependErrorPotential e = prependErrors $ NumErrors $ Set.singleton (e, ErrorPotential)
 
 class CanClearPotentialErrors cnt where
-  {-|
-    If there is a value, remove any potential errors that are associated with it.
-  -}
+  -- |
+  --    If there is a value, remove any potential errors that are associated with it.
   clearPotentialErrors :: cnt -> cnt
 
 instance CanClearPotentialErrors (CN t) where
   clearPotentialErrors (CollectErrors (Just v) (NumErrors es)) =
     CollectErrors (Just v) (NumErrors $ Set.filter notPotential es)
     where
-    notPotential (_, ErrorPotential) = False
-    notPotential _ = True
+      notPotential (_, ErrorPotential) = False
+      notPotential _ = True
   clearPotentialErrors ce = ce
 
-instance (CanClearPotentialErrors t1, CanClearPotentialErrors t2) => CanClearPotentialErrors (t1,t2) where
-  clearPotentialErrors (v1,v2) = (clearPotentialErrors v1, clearPotentialErrors v2)
+instance (CanClearPotentialErrors t1, CanClearPotentialErrors t2) => CanClearPotentialErrors (t1, t2) where
+  clearPotentialErrors (v1, v2) = (clearPotentialErrors v1, clearPotentialErrors v2)
 
 instance (CanClearPotentialErrors t) => CanClearPotentialErrors [t] where
   clearPotentialErrors = map clearPotentialErrors
 
-liftCN  :: (a -> (CN c)) -> (CN a) -> (CN c)
+liftCN :: (a -> (CN c)) -> (CN a) -> (CN c)
 liftCN = liftCE
 
-lift2CN  :: (a -> b -> (CN c)) -> (CN a) -> (CN b) -> (CN c)
+lift2CN :: (a -> b -> (CN c)) -> (CN a) -> (CN b) -> (CN c)
 lift2CN = lift2CE
 
-lift1TCN  :: (a -> b -> (CN c)) -> (CN a) -> b -> (CN c)
+lift1TCN :: (a -> b -> (CN c)) -> (CN a) -> b -> (CN c)
 lift1TCN = lift1TCE
-liftT1CN  :: (a -> b -> (CN c)) -> a -> (CN b) -> (CN c)
+
+liftT1CN :: (a -> b -> (CN c)) -> a -> (CN b) -> (CN c)
 liftT1CN = liftT1CE
 
 type CanTakeCNErrors = CanTakeErrors NumErrors
